3a267309c9c110d2292beac4aaa5e3e3df9900a2
kavdim <kaverzin.dmitriy@gmail.com>
2018-03-19 20:07:29 +0700

    Начало программной логики )))

--- addFiles.py
+++ addFiles.py
@@ -4,39 +4,39 @@ import xml.etree.ElementTree as ET
 import postgresql
 
 # Проверяем наличие файлов, при отсутствии создаем их
+def addFiles():
+    if isfile('delObjects.xml') != True:    # объекты не участвующие в отчетах
+        listObjects = ET.Element('listObjects')
+        listItems = ET.SubElement(listObjects, 'listItems')
 
-if isfile('delObjects.xml') != True:    # объекты не участвующие в отчетах
-    listObjects = ET.Element('listObjects')
-    listItems = ET.SubElement(listObjects, 'listItems')
+        myList = ET.tostring(listObjects)
+        file = open('delObjects.xml', 'w', encoding="utf-8")
+        file.write('<?xml version="1.0" encoding="utf-8" standalone="no" ?>\n')
+        file.write(str(myList))
+        file.close()
 
-    myList = ET.tostring(listObjects)
-    file = open('delObjects.xml', 'w', encoding="utf-8")
-    file.write('<?xml version="1.0" encoding="utf-8" standalone="no" ?>\n')
-    file.write(str(myList))
-    file.close()
+    if isfile('listGroup.xml') != True: #Управление группами
+        db = postgresql.open('pq://root:root@localhost:5432/mgs')
+        query = db.query('SELECT objectgroup.group_number, objectgroup.name FROM objectgroup')
 
-if isfile('listGroup.xml') != True: #Управление группами
-    db = postgresql.open('pq://root:root@localhost:5432/mgs')
-    query = db.query('SELECT objectgroup.group_number, objectgroup.name FROM objectgroup')
+        listGroup = ET.Element('listGroup')
 
-    listGroup = ET.Element('listGroup')
+        for s in query:
+            groupItems = ET.SubElement(listGroup,'groupItems')
+            groupItems.set('ID',str(s[0]))
+            var = ET.SubElement(groupItems, 'var')
+            var.set('name', str(s[0]))
+            var.text = str(s[0])
+            var = ET.SubElement(groupItems, 'var')
+            var.set('name', str(s[1]))
+            var.text = str(s[1])
+            var = ET.SubElement(groupItems, 'var')
+            var.set('name', 'Enabled')
+            var.text = 'False'
 
-    for s in query:
-        groupItems = ET.SubElement(listGroup,'groupItems')
-        groupItems.set('ID',str(s[0]))
-        var = ET.SubElement(groupItems, 'var')
-        var.set('name', str(s[0]))
-        var.text = str(s[0])
-        var = ET.SubElement(groupItems, 'var')
-        var.set('name', str(s[1]))
-        var.text = str(s[1])
-        var = ET.SubElement(groupItems, 'var')
-        var.set('name', 'Enabled')
-        var.text = 'False'
+        myGroup = ET.tostring(listGroup).decode('utf-8')
 
-    myGroup = ET.tostring(listGroup).decode('utf-8')
-
-    file = open('listGroup.xml', 'w', encoding='utf-8')
-    file.write('<?xml version="1.0" encoding="utf-8" standalone="no" ?>\n')
-    file.write(myGroup)
-    file.close()
+        file = open('listGroup.xml', 'w', encoding='utf-8')
+        file.write('<?xml version="1.0" encoding="utf-8" standalone="no" ?>\n')
+        file.write(myGroup)
+        file.close()
